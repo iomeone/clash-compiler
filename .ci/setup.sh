80db7103400d400e8d92383ce7a7c77715f75650
Leon Schoorl <leon@qbaylogic.com>
2019-03-12 14:07:20 +0100

    Disable doctests on HEAD

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -14,7 +14,7 @@ ghc --version
 cp .ci/cabal.project.local .
 
 case "$GHC" in
-  ghc-8.6* )
+  ghc-8.6*|ghc-head)
     echo "doctests are broken on GHC 8.6, disabling"
     cp .ci/cabal.project.local-8.6 cabal.project.local
     ;;
